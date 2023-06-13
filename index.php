4ea6a6fe3538bd9c8a956f9a0061935d65d1a69f
Eliton Camargo <camargoliveira@gmail.com>
2023-06-13 09:20:11 -0300

    Base aula 13/06/2023

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-header('Content-type: application/json');
-require_once './controller/index.php';
+//header('Content-type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+$dadosEnviados = file_get_contents('php://input');
+$dadosEnviados = json_decode($dadosEnviados, true);
+$url = explode('/', $_GET['url']);
+echo json_encode([
+    'metodo'=>$method,
+    'url' => $url,
+    'dados' => $dadosEnviados
+]);
