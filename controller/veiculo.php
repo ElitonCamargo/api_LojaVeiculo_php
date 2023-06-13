39e553a29ec4bf72d70a458aa9ce8a8f6dcabe71
Eliton Camargo <camargoliveira@gmail.com>
2023-06-13 10:50:05 -0300

    Capturando dados no controller veiculo

--- controller/veiculo.php
+++ controller/veiculo.php
@@ -1,10 +1,32 @@
 <?php
+switch($method){
+    case "GET":{
+        echo json_encode(["method"=>"GET"]);
+    }
+    break;
+    case "POST":{
+        echo json_encode(["method"=>"POST"]);
+    }
+    break;
+    case "PUT":{
+        echo json_encode(["method"=>"PUT"]);
+    }
+    break;
+    case "DELETE":{
+        echo json_encode(["method"=>"DELETE"]);
+    }
+    break;
+    default:{
+        echo json_encode(["method"=>"ERRO"]);
+    }
+    break;
+}
 
-echo json_encode(
-    [
-        "arquivo"=>"veículo",
-        "method" =>$method,
-        "dadosRecebidos"=>$dadosRecebidos,
-        "url"=>$url    
-    ]
-);
+// echo json_encode(
+//     [
+//         "arquivo"=>"veículo",
+//         "method" =>$method,
+//         "dadosRecebidos"=>$dadosRecebidos,
+//         "url"=>$url    
+//     ]
+// );
