8ee3258465bbdb1bb99f8d9ef834fa417420f726
Eliton Camargo <camargoliveira@gmail.com>
2023-06-14 20:39:25 -0300

    Simplificando as rotas

--- controller/index.php
+++ controller/index.php
@@ -1,7 +1,21 @@
 <?php
+# Os dados de requisição enviados para a API estão sendo salvos dentro da constante GET
 define('GET',[
     'method' => $_SERVER['REQUEST_METHOD'],
     'dadosRecebidos' => json_decode(file_get_contents('php://input')),
     'url' => explode('/', $_GET['url'])
 ]);
-require_once 'router.php';
+# Definindo todos as contas possíveis da aplicação;
+$rotas = [
+    "veiculo"   => "veiculo.php",
+    "montadora" => "montadora.php",
+    "categoria" => "categoria.php"
+];
+# Direcionando a aplicação para a rota especifica.
+$rota = GET['url'][0];
+if(array_key_exists($rota,$rotas)){
+    require_once $rotas[$rota];
+}
+else{
+    require_once 'erro404.php';
+}
