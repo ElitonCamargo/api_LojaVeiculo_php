7588ed6d5974febefefcef818703efe0a7c7be07
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2023-10-16 11:56:17 -0300

    Aula 16/10/23

--- controller/upload.php
+++ controller/upload.php
@@ -1,20 +1,21 @@
 <?php
-require_once 'model/Upload.php';
-$result=[];
-foreach($_FILES as $key => $arquivo){
-    if(is_array($arquivo['name'])){ 
-        $result[$key] = [];     
-        foreach ($arquivo['name'] as $value) {
-            $result[$key][] = $value; 
-            // array_push($result[$key], $value);
-        }
-    }
-    else{
-        $result[$key] = $arquivo['name'];
-    }
-}
+var_dump($_FILES);
+// require_once 'model/Upload.php';
+// $result=[];
+// foreach($_FILES as $key => $arquivo){
+//     if(is_array($arquivo['name'])){ 
+//         $result[$key] = [];     
+//         foreach ($arquivo['name'] as $value) {
+//             $result[$key][] = $value; 
+//             // array_push($result[$key], $value);
+//         }
+//     }
+//     else{
+//         $result[$key] = $arquivo['name'];
+//     }
+// }
 
-echo json_encode($result);
+// echo json_encode($result);
 
 
 // $up = new Upload($arquivo,'assets/img');
