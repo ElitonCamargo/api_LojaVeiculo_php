eff4b940f7c4089e70e0f224e3fcf4b8f47849d2
Eliton Camargo de Oliveira <camargoliveira@gmail.com>
2023-11-06 10:27:27 -0300

    Teste de requisição

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-//header('Content-type: application/json');
+header('Content-type: application/json');
+$origin = @$_SERVER['HTTP_ORIGIN'];
+header("Access-Control-Allow-Origin: $origin");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once 'controller/index.php';
--- t.php
+++ t.php
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <input type="file" name="foto" multiple>
-        <input type="file" name="capa" >
-        <input type="submit">
-    </form>
-</body>
-</html>
+<?php
+function div($x,$y, $out){
+    if($y != 0){
+        $out = $x/$y;
+        return true;
+    }
+    $out = 0;
+    return false;
+}
